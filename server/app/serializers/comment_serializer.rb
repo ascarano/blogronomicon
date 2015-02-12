d7edf64f0f8253faa9a2e43756b287553c096d7f
ascarano <adamscarano@gmail.com>
2015-02-12 10:41:19 -0700

    added env

--- server/app/serializers/comment_serializer.rb
+++ server/app/serializers/comment_serializer.rb
@@ -1,6 +1,6 @@
 class CommentSerializer < ActiveModel::Serializer
   attributes :body, :id
 
-  has_one :post, except: [:user, :comments]
+  has_one :post, except: [:user]
   has_one :user, except: [:posts, :comments]
 end
