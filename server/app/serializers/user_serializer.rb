877b0965ac83db78ca230aa214e269ad464cb80a
ascarano <adamscarano@gmail.com>
2015-02-17 17:23:40 -0700

    updated templates and css

--- server/app/serializers/user_serializer.rb
+++ server/app/serializers/user_serializer.rb
@@ -2,6 +2,6 @@ class UserSerializer < ActiveModel::Serializer
   attributes :name, :id
 
   has_many :comments, except: [:post, :user]
-  has_many :posts, except: [:user, :comments]
+  has_many :posts, only: [:title, :id]
 
 end
